23895caf1db7333e5a2be7aade5bc7bdacca8e63
KSK <kurtknosala@gmail.com>
2019-07-29 14:32:27 -0600

    admin submit complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 
-    $('#end-time').pickatime({});
+    $('#new-overtime-submit').click(function(){
+
+        event.preventDefault();
+
+        var department = $('#new-department').val();
+        var date = $('#new-date').val();
+        var startTime = $('#new-start-time').val();
+        var endTime = $('#new-end-time').val();
+
+    })
 
 })
