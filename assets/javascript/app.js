914eb1bad20b2f3ef9d3c796eb1ffa827022355c
KSK <kurtknosala@gmail.com>
2019-08-10 12:55:31 -0600

    features working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,6 +29,10 @@ $(document).ready(function(){
             startTime: startTime,
             endTime: endTime
           });
+          $('#new-department').prop('selectedIndex',0);
+          $('#new-date').val('');
+          $('#new-start-time').val('');
+          $('#new-end-time').val('');
     })
 
     $('#request-submit').click(function(){
@@ -49,6 +53,11 @@ $(document).ready(function(){
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
 
+        $('#user-name').val('');
+        $('#user-department').prop('selectedIndex',0);
+        $('#user-date').val('');
+        $('#user-start-time').val('');
+        $('#user-end-time').val('');
     })
     
     database.ref('PostedOvertime').on('child_added', function(snapshot){
@@ -83,44 +92,96 @@ $(document).ready(function(){
 
     // dataRef.ref().orderByChild("dateAdded").limitToLast(1).on
     database.ref('RequestOvertime').on('child_added', function(snapshot){
-
+        
         var userDateFormat = moment(snapshot.val().userDate).format('MMM Do YYYY');
         var userStartTimeFormat = moment(snapshot.val().userStartTime, 'HH:mm').format('h:mm a');
         var userEndTimeFormat = moment(snapshot.val().userEndTime, 'HH:mm').format('h:mm a');
+        if(!snapshot.val().approval){
+            var adminApprove = $('<tr>').addClass(snapshot.key).append(
+                $('<td>').text(snapshot.val().userName),
+                $('<td>').text(snapshot.val().userDepartment),
+                $('<td>').text(userDateFormat),
+                $('<td>').text(userStartTimeFormat),
+                $('<td>').text(userEndTimeFormat)
+            )
 
-        var adminApprove = $('<tr>').addClass(snapshot.key).append(
-            $('<td>').text(snapshot.val().userName),
-            $('<td>').text(snapshot.val().userDepartment),
-            $('<td>').text(userDateFormat),
-            $('<td>').text(userStartTimeFormat),
-            $('<td>').text(userEndTimeFormat)
-        )
+            var buttons = $('<td>')
+            var approveButton = $('<button>').text('Approve').addClass('btn btn-success mr-2 approve').attr('value', snapshot.key);
+            var denyButton = $('<button>').text('Deny').addClass('btn btn-danger deny').attr('value', snapshot.key);
 
-        var buttons = $('<td>')
-        var approveButton = $('<button>').text('Approve').addClass('btn btn-success mr-2 approve').attr('value', snapshot.key);
-        var denyButton = $('<button>').text('Deny').addClass('btn btn-danger deny').attr('value', snapshot.key);
+            buttons.append(approveButton);
+            buttons.append(denyButton);
+            adminApprove.append(buttons);
+            $('#overtime-requests').append(adminApprove);
 
-        buttons.append(approveButton);
-        buttons.append(denyButton);
-        adminApprove.append(buttons);
-        $('#overtime-requests').append(adminApprove);
+        }else if(snapshot.val().approval === 'approved'){
+
+            var adminApproved = $('<tr>').addClass(snapshot.key).append(
+                $('<td>').text(snapshot.val().userName),
+                $('<td>').text(snapshot.val().userDepartment),
+                $('<td>').text(userDateFormat),
+                $('<td>').text(userStartTimeFormat),
+                $('<td>').text(userEndTimeFormat)
+            )
+
+            var buttons =$('<td>');
+            var remove = $('<button>').text('Remove').addClass('btn btn-danger').attr({value:snapshot.key, id:'remove'});
+        
+            buttons.append(remove);
+            adminApproved.append(buttons);
+
+            buttons.append(approveButton);
+            buttons.append(denyButton);
+            adminApproved.append(buttons);
+            $('#approved-requests').append(adminApproved);
+
+        }else if(snapshot.val().approval === 'denied'){
+
+            var adminDenied = $('<tr>').addClass(snapshot.key).append(
+                $('<td>').text(snapshot.val().userName),
+                $('<td>').text(snapshot.val().userDepartment),
+                $('<td>').text(userDateFormat),
+                $('<td>').text(userStartTimeFormat),
+                $('<td>').text(userEndTimeFormat)
+            )
+
+            var buttons =$('<td>');
+            var remove = $('<button>').text('Remove').addClass('btn btn-danger').attr({value:snapshot.key, id:'remove'});
+        
+            buttons.append(remove);
+            adminDenied.append(buttons);
+
+            buttons.append(approveButton);
+            buttons.append(denyButton);
+            adminDenied.append(buttons);
+            $('#denied-requests').append(adminDenied);
+        }
     })
 
     $(document).on('click', '#remove', function(){
         
         var childId = this.value;
         database.ref('PostedOvertime').child(childId).remove();
+        database.ref('RequestOvertime').child(childId).remove();
         location.reload();
     })
 
     $(document).on('click', '.approve', function(){
         var valueId = this.value;
-        $('.' + valueId).addClass('approvedTime');
+        var approval = 'approved';
+        database.ref('/RequestOvertime').child(valueId).update({
+            approval: approval
+        });
+        location.reload();
     })
 
     $(document).on('click', '.deny', function(){
         var valueId = this.value;
-        $('.' + valueId).addClass('deniedTime');
+        var approval = 'denied';
+        database.ref('/RequestOvertime').child(valueId).update({
+            approval: approval
+        });
+        location.reload();
     })
 
 
